f37ea9f7ff0968018cebbb4c36b91979336b1d09
manikanta <kanurimani596@gmail.com>
2026-03-09 19:06:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navigation from "./components/Navigation"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
+function App() {
+  return (
+    <BrowserRouter>
+
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
